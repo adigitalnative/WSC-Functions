e5245c686785cfb36b2fbbc983dd0d2327f30fd8
Jacqueline Chenault <adigitalnative@gmail.com>
2017-11-29 11:01:37 -0500

    Basic Plugin creation

---
+++ avantbard-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+  Plugin Name: Site plugin for www.avantbard.org
+  Description: Site specific code changes for WSC Avant Bard's website, www.avantbard.org
+*/
+
+/* Start adding functions below this line */
+
+add_post_type_support( 'page', 'excerpt' );
+
+/* Stop adding functions below this line */
+?>
